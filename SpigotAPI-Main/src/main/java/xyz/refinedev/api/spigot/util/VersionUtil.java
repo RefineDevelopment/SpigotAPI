a6daa223996daaf5e10a6cd6e0e11d43225bf283
DevDrizzy <fistergaming123@gmail.com>
2024-09-29 12:45:57 +0500

    Fixed: 1.21 support
    Signed-off-by: DevDrizzy <fistergaming123@gmail.com>

--- SpigotAPI-Main/src/main/java/xyz/refinedev/api/spigot/util/VersionUtil.java
+++ SpigotAPI-Main/src/main/java/xyz/refinedev/api/spigot/util/VersionUtil.java
@@ -3,17 +3,22 @@ package xyz.refinedev.api.spigot.util;
 import org.bukkit.Bukkit;
 
 public class VersionUtil {
-    public static final String VERSION = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-    public static final int MINOR_VERSION;
 
-    public VersionUtil() {
-    }
+    public static String VERSION;
+    public static int MINOR_VERSION;
 
     public static boolean canHex() {
         return MINOR_VERSION >= 16;
     }
 
     static {
-        MINOR_VERSION = Integer.parseInt(VERSION.split("_")[1]);
+        try {
+            String versionName = Bukkit.getServer().getClass().getPackage().getName();
+            VERSION = versionName.length() < 4 ? versionName.split("\\.")[2] : versionName.split("\\.")[3];
+            MINOR_VERSION = Integer.parseInt(VERSION.split("_")[1]);
+        } catch (Exception e) {
+            VERSION = "v" + Bukkit.getServer().getBukkitVersion().replace("-SNAPSHOT", "").replace("-R0.", "_R").replace(".", "_");
+            MINOR_VERSION = Integer.parseInt(VERSION.split("_")[1]);
+        }
     }
 }
